92b3237d784d03f6435aeb8bc438fe783c55845e
Katarina Mišura <km51482@fer.hr>
2022-07-21 12:55:49 +0200

    UI completed

--- Assets/Scripts/UI/ChangeScene.cs
+++ Assets/Scripts/UI/ChangeScene.cs
@@ -1,18 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ChangeScene : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    public void StartTheGame()
     {
-        
+        SceneManager.LoadScene(1);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void BackToMenu()
     {
-        
+        SceneManager.LoadScene(0);
     }
 }
